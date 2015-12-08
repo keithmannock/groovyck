0b87a0988004a43521260c085628ac72e6021ac8
Keith Mannock <keith@dcs.bbk.ac.uk>
2015-12-08 16:37:39 +0000

    Minor changes to rmi to exclude rmic and RMISecurityManager + add bit about dynamic proxy

---
+++ src/EchoClientLauncher.java
@@ -0,0 +1,32 @@
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
+
+public class EchoClientLauncher {
+
+    public static void main(String[] args) {
+        System.out.print("String to transmit: ");
+        String text = new Scanner(System.in).nextLine();
+
+        // if there is no security manager, start one
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+
+        try {
+            String name = "Echo";
+            Registry registry = LocateRegistry.getRegistry("localhost");
+            EchoService echoService = (EchoService) registry.lookup(name);
+            String receivedEcho = echoService.echo(text);
+            System.out.println("receivedEcho is: " + receivedEcho);
+        } catch (NotBoundException ex) {
+            ex.printStackTrace();
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/EchoServerLauncher.java
@@ -0,0 +1,27 @@
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class EchoServerLauncher {
+
+    public static void main(String[] args) {
+        // if there is no security manager, start one
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            //create the registry if there is not one
+            Registry registry = LocateRegistry.getRegistry();
+
+            //create the server object
+            EchoServer server = new EchoServer();
+
+            //register (bind) the server object on the registry
+            //The registry may be on a different machine
+
+            registry.rebind("Echo", server);
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
